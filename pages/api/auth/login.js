4e7b71bd72a6c0eee89addbd5930850cef348600
loli <monisha.1901116@srec.ac.in>
2022-04-07 17:57:48 +0530

    login backend

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,6 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+    if(req.method!="POST")
+    return res.status(405).json({message:"Only POST method is accepted"})
+    const email=req.body.email
+    const password=req.body.password
+    const result=await prisma.user.findMany({
+        where:{
+            email,password
+        } 
+    })
+    if(result.length===0)
+    return res.status(400).json({message:"email/password is incorrect"})
+    res.status(200).json({message:"Successfully logged in"})
+
+    res.status(200).json({ email })
+
   }
   
