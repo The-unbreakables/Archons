bc134f291f4ff63971c336d8d0a8f3f1a2907239
Ramasubramaniam1512 <74701299+Ramasubramaniam1512@users.noreply.github.com>
2022-04-07 16:06:01 +0530

    frontent..

---
+++ components/events.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Upcoming = () => {
+  return (
+    <div>Upcoming</div>
+  )
+}
+export const Ongoing = () => {
+    return (
+      <div>Upcoming</div>
+    )
+  }
+export const Ended = () => {
+    return (
+      <div>Upcoming</div>
+    )
+  }
