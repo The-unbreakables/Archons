8bb11757b8b912fd8540e59a20f1c9a378dc285b
loli <monisha.1901116@srec.ac.in>
2022-04-07 18:59:10 +0530

    header

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,8 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client'
 
-export default function handler(req, res) {
-    if(req.method==='POST')
-    
+const prisma = new PrismaClient()
+
+
+export default async function handler(req, res) {
+    if(req.method!=='POST')
+    return res.status(405)
+    const email=req.body.email
+    const result=await prisma.user.findMany({
+        where: {
+            email
+        }
+    })
+    if(result.length!==0)
+    return res.status(400).json({message:"This email already exists"})
+    await prisma.user.create({
+        data: {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      })
+
     res.status(200).json({ name: 'John Doe' })
   }
   
